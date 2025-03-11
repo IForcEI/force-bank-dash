d7c6928f8e4d559d4bca1a567411fd9cfb307437
IForcEI <dftruck1@gmail.com>
2025-03-11 18:24:49 +0300

    Все вернул как было

--- src/app/routers/routers.jsx
+++ src/app/routers/routers.jsx
@@ -1,52 +1,47 @@
-import { Accounts } from '@pages/Accounts/ui/Accounts'
-import { CreditCards } from '@pages/CreditCards/ui/CreditCards'
-import { Home } from '@pages/Home/ui/Home'
-import { Investments } from '@pages/Investments/ui/Investments'
-import { Loans } from '@pages/Loans/ui/Loans'
-import { MyPrivileges } from '@pages/MyPrivileges/ui/MyPrivileges'
-import { Notfound } from '@pages/Notfound/ui/Notfound'
-import { Services } from '@pages/Services/ui/Services'
-import { Setting } from '@pages/Setting/ui/Setting'
-import { Transactions } from '@pages/Transactions/ui/Transactions'
+import * as Pages from '@pages/'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <Home />,
-		errorElement: <Notfound />,
+		element: <Pages.Home />,
+		errorElement: <Pages.Notfound />,
 		children: [
 			{
+				path: '/Dashboard',
+				element: <Pages.Dashboard />,
+			},
+			{
 				path: '/Transactions',
-				element: <Transactions />,
+				element: <Pages.Transactions />,
 			},
 			{
 				path: '/Accounts',
-				element: <Accounts />,
+				element: <Pages.Accounts />,
 			},
 			{
 				path: '/Investments',
-				element: <Investments />,
+				element: <Pages.Investments />,
 			},
 			{
 				path: '/CreditCards',
-				element: <CreditCards />,
+				element: <Pages.CreditCards />,
 			},
 			{
 				path: '/Loans',
-				element: <Loans />,
+				element: <Pages.Loans />,
 			},
 			{
 				path: '/Services',
-				element: <Services />,
+				element: <Pages.Services />,
 			},
 			{
 				path: '/MyPrivileges',
-				element: <MyPrivileges />,
+				element: <Pages.MyPrivileges />,
 			},
 			{
 				path: '/Setting',
-				element: <Setting />,
+				element: <Pages.Setting />,
 			},
 		],
 	},
